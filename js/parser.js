9fce5be4653350353517c40e19f658c5eeaebed5
Matt Hansen <mhansendev@gmail.com>
2017-02-13 02:10:35 -0500

    Project description and about content stored in json

---
+++ js/parser.js
@@ -0,0 +1,35 @@
+readFile("/../json/content.json");
+
+function readFile(file)
+{
+	var f = new XMLHttpRequest();
+	f.open("GET", file, false);
+	f.onreadystatechange = function()
+	{
+		if (f.readyState === 4 && (f.status === 200 || f.status == 0))
+		{
+			var obj = JSON.parse(f.responseText);
+			
+			// About section
+			document.getElementById(obj.personal.ids[0]).innerHTML=obj.personal["name"];
+			document.getElementById(obj.personal.ids[1]).innerHTML=obj.personal["profession"];
+			document.getElementById(obj.personal.ids[2]).innerHTML=obj.personal["bio"]
+			
+			// Project section
+			Object.entries(obj.projects).forEach(
+				([index, project]) => {
+					document.getElementById(project.ids[0]).innerHTML=project["title"];
+					document.getElementById(project.ids[1]).innerHTML=project["description"];
+
+					// Create tags
+					Object.entries(project.tags).forEach(
+						([i, tag]) => {
+							document.getElementById(project.ids[2]).innerHTML+="<div class=\"tag\">" + tag + "</div>";
+						}
+					);
+				}
+			);
+		}
+	}
+	f.send(null);
+}
